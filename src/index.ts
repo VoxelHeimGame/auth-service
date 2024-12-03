2fd2e0ad3c5dc440ebfacb24cec637ca4b4a1b6b
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-12-03 23:48:01 +0100

    chore: add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Auth Service is running').listen(3000)
+const app = new Elysia().get('/', () => 'Auth Service is running').listen(3001)
 
 console.log(`🔒 Auth Service is running at ${app.server?.hostname}:${app.server?.port}`)
