530d79bd6e2e8a222d52732db435c9b55581e249
Ivan Cavero <dev@ivancavero.com>
2024-12-15 13:43:47 +0100

    feat: add health enpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Auth Service is running').listen(3001)
+const app = new Elysia()
+	.get('/', () => 'Auth Service is running')
+	.get('/health', () => ({ status: 'ok' }))
+	.listen({
+		hostname: '0.0.0.0',
+		port: process.env.API_PORT ? Number.parseInt(process.env.API_PORT) : 3001,
+		reusePort: true
+	})
 
 console.log(`🔒 Auth Service is running at ${app.server?.hostname}:${app.server?.port}`)
